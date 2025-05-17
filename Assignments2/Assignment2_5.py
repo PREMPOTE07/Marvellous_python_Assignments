b93730889709ec01384ddbe8f7ead4ebc0df32a5
PREMPOTE07 <prempote07@gmail.com>
2025-05-17 15:27:33 +0530

    Update Assignment2_5.py

--- Assignments2/Assignment2_5.py
+++ Assignments2/Assignment2_5.py
@@ -1,8 +1,10 @@
-def ChkPrime(num):
-    for i in range(2, int(num**0.5) + 1):
+def CheckPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2,num):
         if num % i == 0:
-            return False  
-    return True 
+            return False
+    return True
 
 def main():
     no = int(input("Enter the Number: "))

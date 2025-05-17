a0d02d74db3ce414f37061d6156052d3b6a1ff08
PREMPOTE07 <prempote07@gmail.com>
2025-05-17 15:27:03 +0530

    Update Assignment4_5.py

--- Assignments4/Assignment4_5.py
+++ Assignments4/Assignment4_5.py
@@ -1,10 +1,9 @@
 def CheckPrime(num):
-    if num > 1:
-     for i in range(2,int(num ** 0.5) + 1):
+    if num <= 1:
+        return False
+    for i in range(2,num):
         if num % i == 0:
             return False
-    else:
-        return False
     return True
 
 MultiplyBT = lambda a : a  * 2
